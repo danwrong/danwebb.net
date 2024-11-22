12af279febf355d6cf36d3acf2933f1f6997bb34
Dan Webb <dan.webb@existent.com>
2024-11-22 02:24:58 +0000

    added atom feed

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,4 +1,5 @@
 import date from "nunjucks-date"
+import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 
 export default function (config) {
     config.addPassthroughCopy("src/css");
@@ -12,4 +13,23 @@ export default function (config) {
     config.addFilter("date", date);
 
     config.addGlobalData("now", new Date());
+
+    config.addPlugin(feedPlugin, {
+        type: "atom", // or "rss", "json"
+        outputPath: "/feed.xml",
+        collection: {
+            name: "blog", // iterate over `collections.posts`
+            limit: 10,     // 0 means no limit
+        },
+        metadata: {
+            language: "en",
+            title: "danwebb.net",
+            subtitle: "The personal site of Dan Webb aka @danwrong.",
+            base: "https://danwebb.net/",
+            author: {
+                name: "Dan Webb",
+                email: "dan@danwebb.net",
+            }
+        }
+    });
 }
